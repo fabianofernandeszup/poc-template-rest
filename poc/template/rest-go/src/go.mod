9c381071245a8208cdb4ea03b1136a046c267bd9
GuillaumeFalourd <guillaume.falourd@zup.com.br>
2021-08-12 18:07:10 -0300

    refactor formula.go data to map string interface
    Signed-off-by: GuillaumeFalourd <guillaume.falourd@zup.com.br>

--- poc/template/rest-go/src/go.mod
+++ poc/template/rest-go/src/go.mod
@@ -5,6 +5,8 @@ go 1.14
 require (
 	github.com/Jeffail/gabs v1.4.0
 	github.com/kr/text v0.2.0 // indirect
+	github.com/mattn/go-colorable v0.1.8 // indirect
+	github.com/mgutz/ansi v0.0.0-20200706080929-d51e80ef957d
 	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
 	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
 	gopkg.in/yaml.v2 v2.4.0
--- poc/template/rest-go/src/pkg/formula/formula.go
+++ poc/template/rest-go/src/pkg/formula/formula.go
@@ -10,7 +10,9 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"reflect"
 	"regexp"
+	"strconv"
 	"strings"
 
 	"github.com/Jeffail/gabs"
@@ -49,46 +51,73 @@ type (
 	}
 
 	Step struct {
-		Name    string            `yaml:"name"`
-		Method  string            `yaml:"method"`
-		URL     string            `yaml:"url"`
-		Output  map[string]string `yaml:"output"`
-		Headers map[string]string `yaml:"headers,omitempty"`
-		Data    map[string]string `yaml:"data,omitempty"`
+		Name    string                 `yaml:"name"`
+		Method  string                 `yaml:"method"`
+		URL     string                 `yaml:"url"`
+		Output  map[string]string      `yaml:"output"`
+		Headers map[string]string      `yaml:"headers,omitempty"`
+		Data    map[string]interface{} `yaml:"data,omitempty"`
 	}
 )
 
-func check(e error) {
+func check(e error, m string) {
 	if e != nil {
+		if m != "" {
+			fmt.Println(m)
+		}
 		panic(e)
+	}
+}
+
+func TypeOf(v interface{}) string {
+	return reflect.TypeOf(v).String()
+}
+
+func Verbose(v bool, i interface{}, m string) {
+	if v {
+		if m != "" {
+			fmt.Println("\n" + m)
+		}
+		fmt.Println(i)
 	}
 }
 
 func Run() {
 	if Exists("formula.yml") {
+
 		yamlFile, err := ioutil.ReadFile("formula.yml")
 		y := FormulaYAML{}
 		err = yaml.Unmarshal([]byte(yamlFile), &y)
-		check(err)
+		check(err, "ERROR: formula.yml file incorrect format")
+
+		verbose, _ := strconv.ParseBool(os.Getenv("RIT_VERBOSE"))
 		sm := make(map[string]string)
+
+		fmt.Println("\033[0;34m\n🤖 START WORKFLOW EXECUTION ⚙️\033[0m")
+
 		for _, step := range y.Execution.Steps {
-			fmt.Println("\n- - - - - - - - - - Step " + step.Name + " - - - - - - - - - - ")
-			// fmt.Println("\nName:", step.Name)
-			// fmt.Println("Method:", step.Method)
-			// fmt.Println("Url:", step.URL)
-			// fmt.Println("Headers:", step.Headers)
-			// fmt.Println("Data:", step.Data)
-			// fmt.Println("Output:", step.Output)
-			step.URL = convertURLDynamicValues(step.URL, sm)
-			if len(step.Data) != 0 {
-				step.Data = convertMapDynamicValues(step.Data, sm, "data")
+
+			fmt.Println("\033[1;34m\n🤖 " + strings.ToUpper(step.Name) + " STEP\033[0m")
+
+			if step.URL != "" {
+				step.URL = CheckDynamicValues(step.URL, sm).(string)
 			}
+			Verbose(verbose, step.URL, "\033[1mURL 👀\033[0m")
+
 			if len(step.Headers) != 0 {
-				step.Headers = convertMapDynamicValues(step.Headers, sm, "headers")
+				step.Headers = CheckDynamicValues(step.Headers, sm).(map[string]string)
 			}
-			response := consumeAPI(step.Method, step.URL, step.Headers, step.Data, step.Output)
+			Verbose(verbose, step.Headers, "\033[1mHEADERS 👀\033[0m")
+
+			if len(step.Data) != 0 {
+				step.Data = CheckDynamicValues(step.Data, sm).(map[string]interface{})
+			}
+			Verbose(verbose, step.Data, "\033[1mDATA 👀\033[0m")
+
+			response := CallAPI(step.Method, step.URL, step.Headers, step.Data, step.Output)
 			sm[step.Name] = response
 		}
+		fmt.Println("\033[0;34m\n🤖 END OF WORKFLOW EXECUTION 🚀\033[0m")
 	} else {
 		fmt.Println("ERROR: formula.yml file not found")
 	}
@@ -101,113 +130,138 @@ func Exists(path string) bool {
 	return true
 }
 
-func consumeAPI(method string, url string, headers map[string]string, data map[string]string, output map[string]string) string {
+func CallAPI(method string, url string, headers map[string]string, data map[string]interface{}, output map[string]string) string {
 	client := &http.Client{}
 
 	var jsonStr []byte
 	if len(data) != 0 {
 		j, err := json.Marshal(data)
-		check(err)
+		check(err, "ERROR: step.data incorrect format")
 		jsonStr = j
 	}
 
 	req, err := http.NewRequest(strings.ToUpper(method), url, bytes.NewBuffer(jsonStr))
-	check(err)
+	check(err, "ERROR: Building API request")
 
 	for k, v := range headers {
 		req.Header.Add(k, v)
 	}
 
 	resp, err := client.Do(req)
-	check(err)
+	check(err, "ERROR: API call unexpected error")
 
 	defer resp.Body.Close()
 
 	bodyBytes, err := ioutil.ReadAll(resp.Body)
-	check(err)
+	check(err, "ERROR: Couldn't read API response")
 
-	fmt.Println("\nRequest StatusCode:", resp.StatusCode)
-
-	for k, v := range output {
-		if k == "format" {
-			if v == "table" {
-				// Can't use https://github.com/gosuri/uitable or https://github.com/lensesio/tableprinter without response struct
-				// fmt.Println(string(bodyBytes))
-			}
-			if v == "json" {
-				fmt.Println(string(bodyBytes))
-			}
+	if resp.StatusCode >= 300 {
+		fmt.Println("🔴 API Response StatusCode:", resp.StatusCode)
+		panic("")
+	} else {
+		fmt.Println("\n🟢 API Response StatusCode:", resp.StatusCode)
+		if len(bodyBytes) > 0 {
+			jsonParsed, err := gabs.ParseJSON(bodyBytes)
+			check(err, "ERROR: Couldn't store API response")
+			return jsonParsed.String()
 		}
 	}
 
-	if len(bodyBytes) > 0 {
-		jsonParsed, err := gabs.ParseJSON(bodyBytes)
-		check(err)
-		return jsonParsed.String()
-	}
+	// for k, v := range output {
+	// 	if k == "format" {
+	// 		if v == "table" {
+	// 			// Can't use https://github.com/gosuri/uitable or https://github.com/lensesio/tableprinter without response struct
+	// 			// fmt.Println(string(bodyBytes))
+	// 		}
+	// 		if v == "json" {
+	// 			fmt.Println(string(bodyBytes))
+	// 		}
+
+	// 		if v == "template" {
+	// 			jsonParsed, _ := gabs.ParseJSON(bodyBytes)
+	// 			value := jsonParsed.Path("output.data.x").String()
+	// 			value = strings.Replace(value, "\"", "", -1)
+	// 		}
+	// 	}
+	// }
 
 	return ""
 }
 
-func convertMapDynamicValues(data map[string]string, sm map[string]string, mapType string) map[string]string {
-	for k, v := range data {
-		if isExpression(data[k], sm) {
-			data[k] = updateDynamicValues(data[k], sm)
-			re2 := regexp.MustCompile(`\[\[([^{}]+)\]\]`) // Inside Expression Pattern
-			match2 := re2.FindStringSubmatch(data[k])
-			data[k] = match2[1]
-		} else {
-			if strings.Contains(v, "{{") {
-				v = strings.Replace(v, "{{", "", -1)
-				v = strings.Replace(v, "}}", "", -1)
-				data[k] = convertDynamicValues(v, sm)
+func CheckDynamicValues(data interface{}, sm map[string]string) interface{} {
+	var d interface{}
+
+	if TypeOf(data) == "map[string]interface {}" {
+		d, _ := data.(map[string]interface{})
+		for k, _ := range d {
+			d[k] = CheckDynamicValues(d[k], sm)
+		}
+		d = ConvertStructToMapStringInterface(d)
+		return d
+	}
+
+	if TypeOf(data) == "map[string]string" {
+		d, _ := data.(map[string]string)
+		for k, _ := range d {
+			if ContainsDynamicValues(d[k]) {
+				d[k] = UpdateDynamicValues(d[k], sm)
 			}
 		}
+		return d
 	}
-	if len(data) != 0 {
-		if mapType == "data" {
-			fmt.Println("\nUpdated DATA:", data)
+
+	if TypeOf(data) == "string" {
+		d, _ := data.(string)
+		if ContainsDynamicValues(d) {
+			d = UpdateDynamicValues(d, sm)
 		}
-		if mapType == "headers" {
-			fmt.Println("\nUpdated HEADERS:", data)
-		}
+		return d
 	}
-	return data
+
+	fmt.Println("\nData Type not support:", TypeOf(data))
+
+	return d
 }
 
-func convertURLDynamicValues(url string, sm map[string]string) string {
-	if strings.Contains(url, "{{") {
-		url = updateDynamicValues(url, sm)
-		fmt.Println("\nUpdated URL:", url)
+func ContainsDynamicValues(field string) bool {
+	re := regexp.MustCompile(dynamicPattern)
+	match := re.FindAllStringSubmatch(field, 10)
+	if len(match) > 0 {
+		return true
+	} else {
+		return false
 	}
-	return url
 }
 
-func updateDynamicValues(param string, sm map[string]string) string {
+func ConvertStructToMapStringInterface(s map[string]interface{}) map[string]interface{} {
+	sv := reflect.ValueOf(s)
+	msi := make(map[string]interface{})
+	if sv.Kind() == reflect.Map {
+		for _, key := range sv.MapKeys() {
+			strct := sv.MapIndex(key)
+			// fmt.Println("Key:", key.String())
+			// fmt.Println("Interface Structure:", strct.Interface())
+			msi[key.String()] = strct.Interface()
+		}
+	}
+	return msi
+}
+
+func UpdateDynamicValues(param string, sm map[string]string) string {
 	re := regexp.MustCompile(dynamicPattern)
 	match := re.FindAllStringSubmatch(param, 10)
 	if len(match) != 0 {
 		for i := 0; i < len(match); i++ {
 			key := match[i][0]
 			value := match[i][1]
-			newValue := convertDynamicValues(value, sm)
+			newValue := ConvertDynamicValues(value, sm)
 			param = strings.Replace(param, key, newValue, -1)
 		}
 	}
 	return param
 }
 
-func isExpression(field string, sm map[string]string) bool {
-	re1 := regexp.MustCompile(expressionPattern)
-	match := re1.FindStringSubmatch(field)
-	if len(match) == 0 {
-		return false
-	} else {
-		return true
-	}
-}
-
-func convertDynamicValues(value string, sm map[string]string) string {
+func ConvertDynamicValues(value string, sm map[string]string) string {
 	sv := strings.Split(value, ".")
 	if len(sv) != 0 {
 		if sv[0] == "inputs" {
@@ -217,18 +271,16 @@ func convertDynamicValues(value string, sm map[string]string) string {
 		if sv[0] == "steps" {
 			for k, _ := range sm {
 				if k == sv[1] {
-					path := strings.Replace(value, "steps."+sv[1]+".", "", -1)
-					//fmt.Println("Path:", path)
-					//fmt.Println("Json Stored:", sm[k])
+					prefix := "steps." + sv[1] + "."
+					path := strings.Replace(value, prefix, "", -1)
 					jsonParsedObj, _ := gabs.ParseJSON([]byte(sm[k]))
 					value = jsonParsedObj.Path(path).String()
 					value = strings.Replace(value, "\"", "", -1)
-					//fmt.Println("Value:", value)
 				}
 			}
 		}
 	} else {
-		fmt.Println("ERROR: Splitting value")
+		fmt.Println("ERROR: Splitting Dynamic value")
 	}
 	return value
 }
